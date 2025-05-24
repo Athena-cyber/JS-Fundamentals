13a64fe294dd735096f48a92e9b052a566f795c9
Franklin <franklinemekaopara@gmail.com>
2025-05-24 21:26:13 +0100

    Update 3-value_argument.js

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -2,6 +2,8 @@ let myVar = "Best School"
 
 if (myVar.includes('0')){
     console.log('Argument found');
+}else if (myVar.includes(School)){
+    console.log(HTTM)
 }else {
     console.log("No argument")
 }
