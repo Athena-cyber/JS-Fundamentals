a0cd7c84a66d2959f35563324e83c67e5ff41a2d
Franklin <franklinemekaopara@gmail.com>
2025-05-24 23:56:55 +0100

    Update 9-add.js

---
+++ 9-add.js
@@ -0,0 +1,12 @@
+const { argv } = require("node:process");
+const args = argv.slice(2);
+
+if (args[0] && args[1]) {
+  console.log(add(args[0], args[1]));
+} else {
+  console.log(NaN);
+}
+
+function add(a, b) {
+  return Number(a) + Number(b);
+}
