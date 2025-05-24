7d12c547c793dd778b70348ffd0ea8bc987fd96e
Franklin <franklinemekaopara@gmail.com>
2025-05-24 19:51:19 +0100

    Update 2-arguments.js

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,8 +1,8 @@
-let myVar = 'Best School'
+const myVar = 'Best School'
 
 if (myVar.includes('Best') && myVar.includes('School')){
     console.log('Arguments found');
-}else if (myVar.includes('Best'){
+}else if (myVar.includes('Best') || myVar.includes('2')){
     console.log('Argument found');
 } else{
     console.log('No argument');
