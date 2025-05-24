385ab052ee100d077f9cecd90a180ec1e7c96ad0
Franklin <franklinemekaopara@gmail.com>
2025-05-24 20:26:34 +0100

    Update 2-arguments.js

--- 2-arguments.js
+++ 2-arguments.js
@@ -8,23 +8,4 @@ if (myVar.includes('Best') && myVar.includes('School')){
     console.log('No argument');
     }
 
-    let myVar = ['Best'];
 
-if (myVar.includes('Best') && myVar.includes('School')) {
-    console.log('Arguments found');
-} else if (myVar.includes('Best') || myVar.includes('2')) {
-    console.log('Argument found');
-} else {
-    console.log('No argument');
-}
-
-
-let myVar = ['Apple', 'Orange'];
-
-if (myVar.includes('Best') && myVar.includes('School')) {
-    console.log('Arguments found');
-} else if (myVar.includes('Best') || myVar.includes('2')) {
-    console.log('Argument found');
-} else {
-    console.log('No argument');
-}
