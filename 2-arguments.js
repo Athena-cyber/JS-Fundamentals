4f07e680a84bfca8bf49315c6c223d8c2120c17b
Franklin <franklinemekaopara@gmail.com>
2025-05-24 19:31:36 +0100

    Create 2-arguments.js

---
+++ 2-arguments.js
@@ -0,0 +1,9 @@
+let myVar = 'p@ssword'
+
+if (myVar.length >= 8 && myVar.includes('@')){
+    console.log('Arguments found');
+} else if (myVar.length >= 8 || myVar.includes('@')){
+    console.log('Argument found');
+}else {
+    console.log('No argument');
+}
