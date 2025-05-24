5d32914394c066462c4f0e41a27d5463d323fb5b
Franklin <franklinemekaopara@gmail.com>
2025-05-24 23:52:54 +0100

    Update 7-multi_c.js

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,6 +1,11 @@
-for (let a = 0; a <= 5; a++){
-    console.log("C is cool")
-}
-for (let b = 0; b <= 0; b++){
-    console.log("C is cool")
+const { argv } = require("node:process");
+const args = argv.slice(2);
+
+if (args[0]) {
+  let num = Number(args[0]);
+  let i = 0;
+  while (i < num) {
+    console.log("C is fun");
+    i++;
+  }
 }
