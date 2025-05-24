bcff00392fe06b852df64bc6a11a7d2b621f2b6a
Franklin <franklinemekaopara@gmail.com>
2025-05-24 19:40:31 +0100

    Update 2-arguments.js

--- 2-arguments.js
+++ 2-arguments.js
@@ -2,10 +2,15 @@ let myVar = 'p@ssword'
 
 if (myVar.length >= 8 && myVar.includes('@')){
     console.log('Arguments found');
-} else if (myVar.length >= 8 || myVar.includes('@')){
-    let myVar = 'password'
-    console.log('Argument found');
-}else {
-    let myVar = 'pass'
-    console.log('No argument');
 }
+
+  let myVar1 = 'password'
+if (myVar1.length >= 8 || myVar1.includes('@')){
+    console.log('Argument found');
+}
+
+    let myVar2 = 'pass'
+   
+    if (myVar2.length <= 8) {
+    console.log('No argument');
+    }
