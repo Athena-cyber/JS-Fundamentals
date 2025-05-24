a29607bca411e6c2f46a60aa91c1de4cb52fac3e
Franklin <franklinemekaopara@gmail.com>
2025-05-24 23:32:03 +0100

    Update 2-arguments.js

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,9 +1,9 @@
 const myVar = ('Best School');
- if (myVar === 'Best School')){
+ if (myVar === 'Best School'){
     console.log('Arguments found');
 } else if (myVar == "Best School"){
     console.log('Argument found');
-} else if{myVar !== "Bougie"
+} else if (myVar !== "Bougie"){
     console.log('No argument');
 }else{
     console.log(print)
